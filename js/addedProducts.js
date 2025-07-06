8ceb46fb981ccd418f14499e52369f69fe366536
Alena Maliushchenko <alenamaliushchenko@gmail.com>
2025-07-06 22:03:22 +0300

    changed link json file

--- js/addedProducts.js
+++ js/addedProducts.js
@@ -1,7 +1,7 @@
 
 export async function loadProducts(){
     try {
-        const res = await fetch ('../data/products.json');
+        const res = await fetch ('./data/products.json');
         return await res.json();
     } catch (error){
         console.log('Помилка завантаження продуктів:', error);
